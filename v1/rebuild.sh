51274f6eb19be14db3c89073ab59a77e8d218b9f
YusufGkts1 <yusuf.goktas@kant.ist>
2021-12-20 17:15:08 +0300

    1

--- v1/rebuild.sh
+++ v1/rebuild.sh
@@ -1,12 +1,12 @@
-docker rm -f otelika-api-yusuf || true
-docker rm -f otelika-db-yusuf || true
-docker rm -f otelika-db-polling-yusuf || true
-docker rm -f otelika-phpmyadmin-yusuf || true
-docker rm -f otelika-redis-yusuf || true
-docker rm -f otelika-redocly-yusuf || true
-docker rm -f otelika-postgis-yusuf || true
-docker rm -f otelika-pgadmin-yusuf || true
-docker rm -f otelika-invoker-event-yusuf || true
-docker rm -f otelika-invoker-polling-queue-yusuf || true
+docker rm -f otelika-api || true
+docker rm -f otelika-db || true
+docker rm -f otelika-db-polling || true
+docker rm -f otelika-phpmyadmin || true
+docker rm -f otelika-redis || true
+docker rm -f otelika-redocly || true
+docker rm -f otelika-postgis || true
+docker rm -f otelika-pgadmin || true
+docker rm -f otelika-invoker-event || true
+docker rm -f otelika-invoker-polling-queue || true
 
-docker-compose -p otelika-yusuf up --build -d
+docker-compose -p otelika up --build -d
